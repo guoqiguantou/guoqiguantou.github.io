ef2bbc6ac39b9282788494c279feeaa4f84a1851
guoqiguantou <1740086582@qq.com>
2019-05-25 17:33:22 +0800

    说明

---
+++ precache-manifest.2ac0accffa5dfc720e5312d603ebc6e4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "20a8b334b666c4dac6ca",
+    "url": "/static/js/6.96c8ba48.chunk.js"
+  },
+  {
+    "revision": "653af258d192af9ae59b",
+    "url": "/static/css/main.7b006eb4.chunk.css"
+  },
+  {
+    "revision": "a83a5751989175c652f9",
+    "url": "/static/js/runtime~main.56edcfc4.js"
+  },
+  {
+    "revision": "8f62c5fd35a3bc9e087a",
+    "url": "/static/js/2.9daaa73a.chunk.js"
+  },
+  {
+    "revision": "d79c8a151558f7be986f",
+    "url": "/static/js/10.78db4a44.chunk.js"
+  },
+  {
+    "revision": "87cc5160238d62c3d917",
+    "url": "/static/js/3.4b979d7f.chunk.js"
+  },
+  {
+    "revision": "aee9cf3b24f50a84b8be",
+    "url": "/static/js/4.e47194cd.chunk.js"
+  },
+  {
+    "revision": "ea0678b82e7172cbc7a5",
+    "url": "/static/js/9.406e0254.chunk.js"
+  },
+  {
+    "revision": "30af154bb8d6858793b6",
+    "url": "/static/js/5.a6259233.chunk.js"
+  },
+  {
+    "revision": "21ad84dc8b68be161236",
+    "url": "/static/js/8.36299bfe.chunk.js"
+  },
+  {
+    "revision": "653af258d192af9ae59b",
+    "url": "/static/js/main.c464499d.chunk.js"
+  },
+  {
+    "revision": "98dea474520fa32cb0e9",
+    "url": "/static/js/7.c0e685db.chunk.js"
+  },
+  {
+    "revision": "ea0678b82e7172cbc7a5",
+    "url": "/static/css/9.ca0cc7f4.chunk.css"
+  },
+  {
+    "revision": "21ad84dc8b68be161236",
+    "url": "/static/css/8.cd1ebc46.chunk.css"
+  },
+  {
+    "revision": "98dea474520fa32cb0e9",
+    "url": "/static/css/7.ff16847a.chunk.css"
+  },
+  {
+    "revision": "20a8b334b666c4dac6ca",
+    "url": "/static/css/6.9aa9f534.chunk.css"
+  },
+  {
+    "revision": "30af154bb8d6858793b6",
+    "url": "/static/css/5.dd3200d7.chunk.css"
+  },
+  {
+    "revision": "87cc5160238d62c3d917",
+    "url": "/static/css/3.f5236c5d.chunk.css"
+  },
+  {
+    "revision": "d79c8a151558f7be986f",
+    "url": "/static/css/10.57e2a6b9.chunk.css"
+  },
+  {
+    "revision": "787f0325b88303aedca969fa7572c452",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dianping/precache-manifest.2f6c7a5ccb797d400d56cb6d329bdc7d.js"
+  "/precache-manifest.2ac0accffa5dfc720e5312d603ebc6e4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dianping/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
